f3c2bc3ec147b1c5df92cd07fc14709f4d44a369
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-06-07 17:28:10 +0530

    If..else

---
+++ Basic Solidity/contracts/controls.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.13;
+
+contract control {
+    uint myNumber = 5;
+
+    function checknum() public returns (bool) {
+
+        if (myNumber < 5) {
+            // do something
+            return false;
+        } else if (myNumber > 5) {
+            // do something
+            return false
+        } else {
+            // do something
+            return true;
+        }
+    }
+}
