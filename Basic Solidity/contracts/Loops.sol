aee20908e756e31ab6e8dbfddec83efe131e7bc2
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-06-07 17:47:11 +0530

    for loop

---
+++ Basic Solidity/contracts/Loops.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.13;
+
+contract loop {
+
+    uint256[] public numbers;
+
+    // constructor to set the value 
+    constructor(uint256[] memory _numbers){
+        numbers = _numbers;
+    }
+
+    function sumWithForLoop() public view returns (uint256){
+        uint256 sum =0;
+
+        for(uint256 i=0; i<numbers.length; i++){
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+
+    // you need to pass that numbers while deploying the contract
+    
+
+
+}
