361ef6c83257b1cb3f0feabb85595707aec1346d
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-06-07 16:09:59 +0530

    BAsic

---
+++ Basic Solidity/hello.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+// contract
+// state variables : to store the data of the contract
+// constructor : to initialize the contract and thier variables
+// function : to define the functions of the contract
+  
+  // 1.  public : can be accessed by anyone
+  // 2.  private : can be accessed only by the contract
+    
+    // view : can be accessed only by the contract and it will not modify the state of the contract
+    // pure : can be accessed only by the contract and it will not modify the state of the contract and it will not read the state of the contract
+    // payable : can be accessed only by the contract and it will not modify the state of the contract and it will not read the state of the contract and it will accept the ether
+
+
+
+
+contract HelloWorld {
+
+    string greeting;
+
+    constructor (){
+        greeting = "Hello Rahul, How are you?";
+    }
+
+    function hello () public view  returns (string memory){
+
+       return greeting;
+    }
+}
