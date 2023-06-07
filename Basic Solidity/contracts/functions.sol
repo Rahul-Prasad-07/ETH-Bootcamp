0b33556a6fd0347bdaa98dc1a736c8ee9fd4421c
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-06-07 18:07:20 +0530

    modifier

--- Basic Solidity/contracts/functions.sol
+++ Basic Solidity/contracts/functions.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 
 contract functions {
 
+    address owner = 0x3AB17f39108541E02953BDf243E1C5295b1D1616;
+
     uint256 myNumber =5;
 
     function addTwoNumber(uint256 _value) public {
@@ -11,7 +13,12 @@ contract functions {
     }
 
     // read only function and return something
-    function getMyNumber() public view returns(uint256){
+    function getMyNumber() public onlyOwner view returns(uint256){
         return myNumber;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender== owner , "You are not the owner");
+        _;
     }
 }
