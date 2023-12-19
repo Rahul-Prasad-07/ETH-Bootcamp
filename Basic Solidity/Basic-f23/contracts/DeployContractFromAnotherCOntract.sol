bbe030f0a9dcaedc4087bac4d6d7aa5a697d0e8c
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-12-19 13:48:07 +0530

    deploying multiple contracts & Interacting with it

--- Basic Solidity/Basic-f23/contracts/DeployContractFromAnotherCOntract.sol
+++ Basic Solidity/Basic-f23/contracts/DeployContractFromAnotherCOntract.sol
@@ -6,14 +6,28 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 // this contract is going to deploy SimpleStorage contract
 contract SimpleStorageFactory{
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContract;
 
     // create SimpleStorage contract
     function createSimpleStorageContract() public {
 
         // create new instace of simpleStorage contract via variable simpleStorage
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract  = new SimpleStorage();
+        listOfSimpleStorageContract.push(newSimpleStorageContract);
 
     }
+
+    // interact with simpleStorage contract func
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+
+        SimpleStorage mySimpleStoragge = listOfSimpleStorageContract[_simpleStorageIndex];
+        mySimpleStoragge.store(_newSimpleStorageNumber);
+    }
+
+    // retreive the new number on perticular contract that you have store
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return listOfSimpleStorageContract[_simpleStorageIndex].retrieve();
+    }
+
 
 }
