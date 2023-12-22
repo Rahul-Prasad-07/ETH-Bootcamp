88dbac19ba736181cce2c9df362f0d511c038188
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-12-22 13:48:15 +0530

    Gas optimization- Constant and Immutable

--- Basic Solidity/Basic-f23/contracts/fund/fundMe.sol
+++ Basic Solidity/Basic-f23/contracts/fund/fundMe.sol
@@ -5,22 +5,27 @@ pragma solidity ^0.8.19;
 import {PriceConverter} from "./PriceConverter.sol";
 
 // sepolia deployed contract address : 0x8942f9F9f7066082B37dd895D3b944B9Ae7040E6 --> With withdrawal-onlyOwner
+
+//810086 
+//790116 --> 766549
 contract FundMe{
 
     using  PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5 * 1e18; // we need to update this in decimal bcz eth price also comes with decimals
+    uint256 public constant MINIMUM_USD = 5 * 1e18; // we need to update this in decimal bcz eth price also comes with decimals
+    // 2451 - non-constant
+    // 351 - constant
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-    address public owner;
+    address public immutable i_owner; // 444
 
     constructor(){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
 
-        require(msg.value.getConvertsionRateofEthinUsd() >=  minimumUsd, "didn't send enough ETH" );  //.getConvertsionRateofEthinUsd(XXXX,YYYY) : here first value xx is passed by msg.value and secound YYY pases in ()
+        require(msg.value.getConvertsionRateofEthinUsd() >=  MINIMUM_USD, "didn't send enough ETH" );  //.getConvertsionRateofEthinUsd(XXXX,YYYY) : here first value xx is passed by msg.value and secound YYY pases in ()
         funders.push(msg.sender); // keep trake of senders
         addressToAmountFunded[msg.sender]  += msg.value; // keep track of senders with value
 
@@ -42,7 +47,7 @@ contract FundMe{
     } 
 
     modifier onlyOwner(){
-        require(msg.sender == owner, "Sender is not owner!");
+        require(msg.sender == i_owner, "Sender is not owner!");
         _;
     } 
 
